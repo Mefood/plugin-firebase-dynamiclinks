e3951cb12790f8acc3362baa8603a7dc8d1ebf53
Andreu <andreu@mefood.io>
2023-04-13 13:18:20 +0200

    f

--- www/FirebaseDynamicLinks.js
+++ www/FirebaseDynamicLinks.js
@@ -2,26 +2,26 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "FirebaseDynamicLinks";
 
 module.exports = {
-    getDynamicLink: function() {
-        return new Promise(function(resolve, reject) {
+    getDynamicLink: function () {
+        return new Promise(function (resolve, reject) {
             exec(resolve, reject, PLUGIN_NAME, "getDynamicLink", []);
         });
     },
-    onDynamicLink: function(onSuccess, onError) {
+    onDynamicLink: function (onSuccess, onError) {
         exec(onSuccess, onError, PLUGIN_NAME, "onDynamicLink", []);
     },
-    createDynamicLink: function(params) {
-        return new Promise(function(resolve, reject) {
+    createDynamicLink: function (params) {
+        return new Promise(function (resolve, reject) {
             exec(resolve, reject, PLUGIN_NAME, "createDynamicLink", [params, 0]);
         });
     },
-    createShortDynamicLink: function(params) {
-        return new Promise(function(resolve, reject) {
+    createShortDynamicLink: function (params) {
+        return new Promise(function (resolve, reject) {
             exec(resolve, reject, PLUGIN_NAME, "createDynamicLink", [params, cordova.platformId === "ios" ? 1 : 2]);
         });
     },
-    createUnguessableDynamicLink: function(params) {
-        return new Promise(function(resolve, reject) {
+    createUnguessableDynamicLink: function (params) {
+        return new Promise(function (resolve, reject) {
             exec(resolve, reject, PLUGIN_NAME, "createDynamicLink", [params, cordova.platformId === "ios" ? 2 : 1]);
         });
     }
